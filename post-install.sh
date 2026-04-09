ce834c58a28c6deb736ae2c16af009ed462eeb68
Jason McCormick <jason@mfamily.org>
2026-04-09 19:43:13 -0400

    initial

---
+++ post-install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+LOG=/var/log/post-install.log
+exec > >(tee -a "$LOG") 2>&1
+
+echo "=== ASL3 Post Install Starting ==="
+
+export DEBIAN_FRONTEND=noninteractive
+
+## Install AllStarLink Repo
+curl -L -o /tmp/asl-apt-repos.deb13_all.deb \
+	https://repo.allstarlink.org/public/asl-apt-repos.deb13_all.deb
+dpkg -i /tmp/asl-apt-repos.deb13_all.deb
+rm -f /tmp/asl-apt-repos.deb13_all.deb
+
+## Now Install ASL3
+apt install -y asl3 asl3-menu allmon3 asl3-appliance vim-nox
+
+# Create motd & issue
+echo "" > /etc/issue
+echo "" > /etc/issue.net
+
+# Install non-package files
+pushd /tmp/asl3
+make install
+popd
+
+# Set Firstboot
+touch /asl3-first-boot-needed
+
+# Cleanup
+rm -rf /tmp/asl3
